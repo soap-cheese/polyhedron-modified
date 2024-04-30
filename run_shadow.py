64d06bc0a97a2039732c9fdfa0b133ee84723310
soap-cheese <125205163+soap-cheese@users.noreply.github.com>
2024-04-30 21:58:19 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -7,7 +7,7 @@ from shadow.polyedr import Polyedr
 
 tk = TkDrawer()
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in ["test1", "ccc", "cube", "box", "king", "cow"]:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
